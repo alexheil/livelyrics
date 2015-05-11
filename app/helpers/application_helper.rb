3f70b3ab6537f9f93cd2386eaebfd83d59bf9a64
Alex Heil <aheil4250@aol.com>
2015-05-11 09:15:12 -0400

    Initial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def full_title(page_title = '')
-    base_title = "live:lyrics"
+    base_title = "let:lyrics"
     if page_title.empty?
       base_title
     elsif @artist

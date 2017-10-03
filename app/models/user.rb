816bffe7781bbb2e38b692ba3de339046deae332
alexheil <aheil4250@aol.com>
2017-10-03 13:55:09 -0400

    login

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,33 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :timeoutable
    #:omniauthable, :confirmable, :lockable
 
+  attr_accessor :login
+
   scope :popular, -> {select("users.id, users.slug, users.username, count(lyrics.id) lyrics_count").joins(:lyrics).group("users.id").reorder("lyrics_count desc").limit(10)}
 
   has_many :lyrics
   has_many :comments, dependent: :destroy
 
   validates :username, presence: true, uniqueness: true, length: { maximum: 50 }, format: { with: /\A[a-zA-Z0-9 ]+\Z/i }
+  validate :validate_username
 
   before_save :should_generate_new_friendly_id?, if: :username_changed?
   before_save :downcase_username
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
 
   private
 
